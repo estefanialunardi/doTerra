734296832b82ff565d8c6e8a42169522a486498b
Estefania Mesquita <estefanialunardi@gmail.com>
2023-06-30 08:45:01 -0300

    dictionary for oils

--- main.py
+++ main.py
@@ -50,18 +50,17 @@ st.header("""Descubra o óleo ideal para você""")
 st.write(""" 👇 Escreva a sua queixa para descobrir o óleo com os benefícios necessários""")
 
 
-lavender = ['dores de cabeça', 'dor de cabeça', 'enxaquecas', 'enxaqueca', 'tensão muscular', 'queimadura', 'queimaduras', 'acne', 'espinhas', 'espinha', 'tensões musculares', 'irritação na pele', 'irritações na pele', 'irritação de pele', 'irritações de pele', 'insônia', 'insonia', 'estresse', 'stresse', 'depressao', 'depressão']
+lavender ={ nome: "Lavender", beneficios: ['dores de cabeça', 'dor de cabeça', 'enxaquecas', 'enxaqueca', 'tensão muscular', 'queimadura', 'queimaduras', 'acne', 'espinhas', 'espinha', 'tensões musculares', 'irritação na pele', 'irritações na pele', 'irritação de pele', 'irritações de pele', 'insônia', 'insonia', 'estresse', 'stresse', 'depressao', 'depressão'], image: 'lavanda-1.jpg'} 
 lemon =  ['gripe', 'gripes', 'resfriado', 'resfriados', 'nariz entupido', 'congestão', 'constipação', 'dor de garganta', 'dores de garganta', 'inflamação de garganta', 'mau humor', 'mau-humor', 'tristeza']
 peppermint = ['nausea', 'nauseas', 'náusea', 'náuseas', 'dor de barriga', 'dores de barriga', 'dores abdominais', 'dor abdominal', 'diarreia', 'colon irritavel', 'cólon irritável', 'intestino irritavel', 'intestino irritável', 'dor muscular', 'dores musculares', 'dor de cabeça', 'dores de cabeça', 'dor de cabeca', 'dores de cabeca', 'repelente', 'falta de concentração', 'falta de concentracao', 'nariz entupido', 'congestão nasal']
 oils = [lavender, lemon, peppermint]
-images = {'lavender': 'lavanda-1.jpg', 'lemon': 'lemon.jpg', 'peppermint': 'peppermint.jpg'}
 
 complaint= st.text_input('Escreva sua queixa', placeholder= 'dor de cabeça').lower()
 
 for oil in oils:
-    if complaint in oil:
-        st.text(oil)
-        st.image(images[oil])
+    if complaint in oil[beneficios]:
+        st.text(oil[nome])
+        st.image(oil[image])
 
         st.success("Este é o óleo ideal para você!")
 
